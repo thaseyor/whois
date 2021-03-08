f195303362373f4892ee346d6a67bcd38595106f
thaseyor <56724794+thaseyor@users.noreply.github.com>
2021-03-08 12:56:18 +0600

    ✅ 2/3 ip whoIs

--- src/App.vue
+++ src/App.vue
@@ -59,7 +59,64 @@
             </b-collapse>
           </div></b-tab-item
         >
-        <b-tab-item label="IP"></b-tab-item>
+        <b-tab-item label="IP"
+          ><p class="content">
+            Ваш ip:
+            <basefont @click="copyIp" style="cursor:pointer">
+              {{ localip }}
+            </basefont>
+          </p>
+          <b-field>
+            <b-input
+              @keydown.enter.native="loadIpInfo"
+              v-model="ip"
+              expanded
+            ></b-input>
+            <p class="control">
+              <b-button
+                type="is-primary"
+                label="Search"
+                @click="loadIpInfo"
+              /></p
+          ></b-field>
+          <div>
+            <b-collapse
+              v-for="item in ips"
+              :key="item.title"
+              class="card"
+              animation="slide"
+              aria-id="contentIdForA11y3"
+              v-model="item.isActive"
+            >
+              <template #trigger="props">
+                <div
+                  class="card-header"
+                  role="button"
+                  aria-controls="contentIdForA11y3"
+                >
+                  <p class="card-header-title">
+                    {{ item.title }}
+                  </p>
+                  <a class="card-header-icon">
+                    <b-icon :icon="props.open ? 'menu-down' : 'menu-up'">
+                    </b-icon>
+                    <b-icon
+                      icon="delete"
+                      @click.stop.native="deleteIp(item.title)"
+                    >
+                    </b-icon>
+                  </a>
+                </div>
+              </template>
+
+              <div class="card-content">
+                <div class="content">
+                  <p v-for="m in item.message" :key="m">{{ m }}</p>
+                </div>
+              </div>
+            </b-collapse>
+          </div></b-tab-item
+        >
         <b-tab-item label="Phone number"></b-tab-item>
       </b-tabs>
     </section>
@@ -72,25 +129,36 @@ export default {
   data() {
     return {
       domain: "",
+      ip: "",
       activeTab: 0,
       isLoading: false,
       domains: [],
+      ips: [],
+      localip: "",
     };
   },
-  created() {
+  async created() {
     const domains = localStorage.getItem("domains-list");
     if (domains) {
       this.domains = JSON.parse(domains);
     }
+    const ips = localStorage.getItem("ips-list");
+    if (ips) {
+      this.ips = JSON.parse(ips);
+    }
+    const ipdata = await fetch(`https://ipinfo.io/json?token=2893d17b16a282`);
+
+    const localip = await ipdata.json();
+    this.localip = localip["ip"];
   },
   methods: {
     async loadDomainInfo() {
       if (this.domains.filter((d) => d.title === this.domain).length) {
-        this.errorNotify(`This domain is already added!`);
+        this.notify(`This domain has already been added!`, "is-danger");
         return;
       }
       if (!this.domain) {
-        this.errorNotify(`Empty string!`);
+        this.notify(`Empty string!`, "is-danger");
         return;
       }
       this.isLoading = true;
@@ -98,7 +166,6 @@ export default {
         `https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey=at_LcfLLteDNWP7Yq7vjlfUpWGMtrELY&outputFormat=JSON&domainName=${this.domain}`
       );
       const jsonData = await data.json();
-      console.log(jsonData);
       this.isLoading = false;
       this.domains = [
         ...this.domains,
@@ -112,23 +179,68 @@ export default {
       ];
       this.domain = "";
     },
-    errorNotify(message) {
+    notify(message, type) {
       this.$buefy.toast.open({
         duration: 5000,
         message,
-        type: "is-danger",
+        type,
         queue: false,
       });
+    },
+    async loadIpInfo() {
+      if (this.ips.filter((d) => d.title === this.ip).length) {
+        this.notify(`This IP has already been added!`, "is-danger");
+        return;
+      }
+      if (!this.ip) {
+        this.notify(`Empty string!`, "is-danger");
+        return;
+      }
+      this.isLoading = true;
+      const data = await fetch(
+        `https://ipinfo.io/${this.ip}/json?token=2893d17b16a282`
+      );
+      const jsonData = await data.json();
+      console.log(jsonData);
+      this.isLoading = false;
+      this.ips = [
+        ...this.ips,
+        {
+          title: this.ip,
+          message: [
+            `Country: ${jsonData["country"]}`,
+            `City: ${jsonData["city"]}`,
+            `Region: ${jsonData["region"]}`,
+            `Internet provider ${jsonData["org"]}`,
+            `Hostname: ${jsonData["hostname"]}`,
+            `Location: ${jsonData["loc"]}`,
+            `Timezone: ${jsonData["timezone"]}`,
+          ],
+          isActive: false,
+        },
+      ];
+      this.ip = "";
     },
     deleteDomain(domainToDeleteTitle) {
       this.domains = this.domains.filter(
         (d) => d.title !== domainToDeleteTitle
       );
     },
+    deleteIp(IpToDeleteTitle) {
+      this.ips = this.ips.filter((d) => d.title !== IpToDeleteTitle);
+    },
+    copyIp() {
+      this.notify("IP copied to clipboard", "is-success");
+      var copyText = this.localip;
+      navigator.clipboard.writeText(copyText);
+    },
   },
   watch: {
     domains() {
       localStorage.setItem("domains-list", JSON.stringify(this.domains));
+    },
+    ips() {
+      localStorage.setItem("ips-list", JSON.stringify(this.ips));
     },
   },
 };
